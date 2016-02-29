cb59834c80dd578909f0c90cfe12b3230bd09e5f
Hasan Karahan <hasan.karahan@blackhan.com>
2016-02-29 10:37:53 +0400

    App: SASS integration

--- shuhadaku/app.py
+++ shuhadaku/app.py
@@ -13,7 +13,8 @@ from shuhadaku.view.login import app_login
 from shuhadaku.view.now import app_now
 from shuhadaku.view.static import app_static
 
-import ARGs
+import ARGs, os, sass
+import csscompressor as cc
 
 ###############################################################################
 ###############################################################################
@@ -28,6 +29,26 @@ app_main.mount ('/api', app_api)
 
 if ARGs.get ('debug'):
     app_main.merge (app_debug)
+
+###############################################################################
+###############################################################################
+
+if ARGs.get ('debug') or not ARGs.get ('no_sass'):
+    for path, dns, fns in os.walk('static/css'):
+        for filename in filter(lambda fn: fn.endswith('.scss'), fns):
+
+            scss_path = os.path.join(path, filename)
+            css_path = scss_path.replace('.scss', '.css')
+
+            if not os.path.exists(css_path):
+                with open(css_path, 'w') as css_file:
+
+                    if not ARGs.get ('debug'):
+                        css = cc.compress(sass.compile_file(scss_path))
+                    else:
+                        css = sass.compile_file(scss_path)
+
+                    css_file.write(css)
 
 ###############################################################################
 ###############################################################################
