c5c0d38456f99f2da94641f84e6827850d8f4d8c
Hasan Karahan <hasan.karahan@blackhan.com>
2016-03-27 18:14:36 +0300

    App: conditional robots.txt

--- notex/view/static.py
+++ notex/view/static.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 ###############################################################################
 
-from bottle import Bottle
+from bottle import Bottle, request
 from bottle import static_file
+
+import os
 
 ###############################################################################
 ###############################################################################
@@ -22,6 +24,17 @@ def server_static (path):
 def server_fonts (path):
 
     return static_file (path, root='./static/fonts')
+
+@app.get ('/robots.txt')
+def server_fonts (*args, **kwargs):
+
+    app_dns = os.environ.get ('OPENSHIFT_APP_DNS')
+    req_dns = request.headers.get ('host')
+
+    if app_dns == req_dns:
+        return static_file ('robots-disallow.txt', root='./static/txt')
+    else:
+        return static_file('robots-allow.txt', root='./static/txt')
 
 ###############################################################################
 ###############################################################################
