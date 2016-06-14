9cf7fb7f4bf1aa599bec7314c9eb3d78332ed20f
Hasan Karahan <hasan.karahan@blackhan.com>
2016-06-14 15:40:38 +0400

    App: app.{js=>ts}

--- static/js/app/cookie/cookie.js
+++ static/js/app/cookie/cookie.js
@@ -1,4 +1,4 @@
-var Cookie = (function () {
+window.cookie = (function () {
     function Cookie() {
     }
     Cookie.set = function (name, value, expiry_ms) {
@@ -12,9 +12,13 @@ var Cookie = (function () {
             var expires = 'expires=' + date.toUTCString();
             document.cookie = name + '=' + json + '; ' + expires;
         }
+        return value;
+    };
+    Cookie.prototype.set = function (name, value, expiry_ms) {
+        return Cookie.set(name, value, expiry_ms);
     };
     Cookie.get = function (name, value) {
-        var cookie_name = name + '=', cookies = document.cookie.split(';'), string;
+        var cookies = document.cookie.split(';'), cookie_name = name + '=', string;
         for (var i = 0; i < cookies.length; i++) {
             var cookie = cookies[i];
             while (cookie.charAt(0) === ' ') {
@@ -36,7 +40,9 @@ var Cookie = (function () {
             return value;
         }
     };
+    Cookie.prototype.get = function (name, value) {
+        return Cookie.get(name, value);
+    };
     return Cookie;
 }());
-window.cookie = Cookie;
 //# sourceMappingURL=cookie.js.map
--- static/js/app/function/mine.ts
+++ static/js/app/function/mine.ts
@@ -1,4 +1,4 @@
-function mine(fn:Function) {
+function mine(fn:Function):Function {
     return function () {
         return fn.apply(this, [this].concat(Array.prototype.slice.call(
             arguments
