ef97781a40a7b51befc56644051f5730e624b99c
Hasan Karahan <hasan.karahan@blackhan.com>
2016-07-15 17:51:22 +0400

    App: fixed domain name

--- notex/view/util/generic.py
+++ notex/view/util/generic.py
@@ -10,55 +10,59 @@ import ARGs, os, ujson as JSON
 ###############################################################################
 
 from jinja2 import Environment, FileSystemLoader
-env = Environment (loader=FileSystemLoader('./template'))
+env = Environment(loader=FileSystemLoader('./template'))
 
-if not ARGs.get ('debug'):
-    env.add_extension ('compressinja.html.HtmlCompressor')
+if not ARGs.get('debug'):
+    env.add_extension('compressinja.html.HtmlCompressor')
 
-env.globals.update ({'JSON': JSON})
+env.globals.update({'JSON': JSON})
 
 ###############################################################################
 
-def generic (tpl_name, **kwargs):
-    domain = request.get_header ('host')
-    keywords = get_keywords (tpl_name, domain=domain)
+def generic(tpl_name, **kwargs):
     tpl = env.get_template(tpl_name + '.html')
 
-    return tpl.render (
-        debug=ARGs.get (key='debug'),
-        email=get_mailto (), description=get_description (tpl_name),
-        domain=domain, keywords=keywords, today=date.today (), **kwargs)
+    return tpl.render(
+        debug=ARGs.get(key='debug'),
+        description=get_description(tpl_name),
+        domain=get_domain(),
+        email=get_mailto(),
+        keywords=get_keywords(tpl_name, domain=get_domain()),
+        today=date.today(),
+        **kwargs)
 
 ###############################################################################
 
-def get_mailto ():
+def get_domain():
 
-    local_part = os.environ.get (
+    host = request.get_header('host') or ''
+    return '.'.join(host.split('.')[-2:])
+
+def get_description(view):
+
+    base = '{0}'.format(view.capitalize())
+    if view == 'editor':
+        return os.environ.get('DESCRIPTION_EDITOR', base)
+    else:
+        return os.environ.get('DESCRIPTION', base)
+
+def get_keywords(view, domain):
+
+    base = [domain.lower()]
+    if view == 'editor':
+        return os.environ.get('KEYWORDs_EDITOR', base + ['editor'])
+    else:
+        return os.environ.get('KEYWORDs', base)
+
+def get_mailto():
+
+    local_part = os.environ.get(
         'MAILTO_LOCAL', 'contact')
-    domain_part = os.environ.get (
-        'MAILTO_DOMAIN', request.get_header ('host'))
-    return os.environ.get (
-        'MAILTO', '{0}@{1}'.format (local_part, domain_part))
+    domain_part = os.environ.get(
+        'MAILTO_DOMAIN', request.get_header('host'))
 
-def get_keywords (view, domain):
-
-    base = [domain.lower ()]
-    if view == 'home':
-        return os.environ.get ('KEYWORDs_HOME', base + ['home'])
-    elif view == 'contact':
-        return os.environ.get ('KEYWORDs_CONTACT', base + ['contact'])
-    else:
-        return os.environ.get ('KEYWORDs', base)
-
-def get_description (view):
-
-    base = '{0}: Weblog Service'.format (view.capitalize ())
-    if view == 'home':
-        return os.environ.get ('DESCRIPTION_HOME', base)
-    elif view == 'contact':
-        return os.environ.get ('DESCRIPTION_CONTACT', base)
-    else:
-        return os.environ.get ('DESCRIPTION', base)
+    return os.environ.get(
+        'MAILTO', '{0}@{1}'.format(local_part, domain_part))
 
 ###############################################################################
 ###############################################################################
