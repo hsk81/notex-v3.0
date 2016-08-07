e2dfbf6d248e83094f61f2f78a889d50d03ed640
Hasan Karahan <hasan.karahan@blackhan.com>
2016-08-07 17:21:56 +0400

    Toolbar: undo, redo, cut, copy, paste et al.

--- static/js/app/ui/md-editor-toolbar.js
+++ static/js/app/ui/md-editor-toolbar.js
@@ -17,14 +17,22 @@ define(["require", "exports", '../decorator/named', '../decorator/trace'], funct
                 mouseWheel: true,
                 scrollbars: true
             });
-            $('.glyphicon-scissors').closest('button')
-                .on('click', this.onScissorsClick.bind(this));
-            $('.glyphicon-duplicate').closest('button')
-                .on('click', this.onDuplicateClick.bind(this));
-            $('.glyphicon-paste').closest('button')
-                .on('click', this.onPasteClick.bind(this));
+            $('.glyphicon.undo').closest('button')
+                .on('click', this.onUndoClick.bind(this));
+            $('.glyphicon.redo').closest('button')
+                .on('click', this.onRedoClick.bind(this));
             $('.glyphicon-erase').closest('button')
                 .on('click', this.onEraseClick.bind(this));
+            $('.glyphicon-scissors').closest('button')
+                .on('click', this.onScissorsClick.bind(this));
+            $('.glyphicon-copy').closest('button')
+                .on('click', this.onCopyClick.bind(this));
+            $('.glyphicon-paste').closest('button')
+                .on('click', this.onPasteClick.bind(this));
+            $('.glyphicon-indent-left').closest('button')
+                .on('click', this.onIndentLeftClick.bind(this));
+            $('.glyphicon-indent-right').closest('button')
+                .on('click', this.onIndentRightClick.bind(this));
         }
         Object.defineProperty(MdEditorToolbar, "me", {
             get: function () {
@@ -39,14 +47,96 @@ define(["require", "exports", '../decorator/named', '../decorator/trace'], funct
         MdEditorToolbar.prototype.refresh = function () {
             this._scroll.refresh();
         };
-        MdEditorToolbar.prototype.onScissorsClick = function (ev) {
+        MdEditorToolbar.prototype.onUndoClick = function (ev) {
+            this.$textarea.one('focus', function () {
+                var result = document.execCommand('undo');
+                if (result !== true) {
+                    console.debug('[on:undo]');
+                }
+            });
+            this.$textarea.focus();
         };
-        MdEditorToolbar.prototype.onDuplicateClick = function (ev) {
-        };
-        MdEditorToolbar.prototype.onPasteClick = function (ev) {
+        MdEditorToolbar.prototype.onRedoClick = function (ev) {
+            this.$textarea.one('focus', function () {
+                var result = document.execCommand('redo');
+                if (result !== true) {
+                    console.debug('[on:redo]');
+                }
+            });
+            this.$textarea.focus();
         };
         MdEditorToolbar.prototype.onEraseClick = function (ev) {
+            this.$textarea.one('focus', function () {
+                var result = document.execCommand('delete');
+                if (result !== true) {
+                    console.debug('[on:erase]');
+                }
+            });
+            this.$textarea.focus();
         };
+        MdEditorToolbar.prototype.onScissorsClick = function (ev) {
+            var _this = this;
+            this.$textarea.one('focus', function () {
+                var beg = _this.$textarea[0].selectionStart, end = _this.$textarea[0].selectionEnd, val = _this.$textarea[0].value;
+                _this._text = val.slice(beg, end);
+                var result = document.execCommand('cut');
+                if (result !== true) {
+                    console.debug('[on:cut]');
+                }
+            });
+            this.$textarea.focus();
+        };
+        MdEditorToolbar.prototype.onCopyClick = function (ev) {
+            var _this = this;
+            this.$textarea.one('focus', function () {
+                var beg = _this.$textarea[0].selectionStart, end = _this.$textarea[0].selectionEnd, val = _this.$textarea[0].value;
+                _this.text = val.slice(beg, end);
+                var result = document.execCommand('copy');
+                if (result !== true) {
+                    console.debug('[on:copy]');
+                }
+            });
+            this.$textarea.focus();
+        };
+        MdEditorToolbar.prototype.onPasteClick = function (ev) {
+            var _this = this;
+            this.$textarea.one('focus', function () {
+                var result = document.execCommand('insertText', true, _this.text);
+                if (result !== true) {
+                    console.debug('[on:insertText]');
+                }
+            });
+            this.$textarea.focus();
+        };
+        MdEditorToolbar.prototype.onIndentLeftClick = function (ev) {
+            this.$textarea.one('focus', function () {
+                console.debug('[on:focus]');
+            });
+            this.$textarea.focus();
+        };
+        MdEditorToolbar.prototype.onIndentRightClick = function (ev) {
+            this.$textarea.one('focus', function () {
+                console.debug('[on:focus]');
+            });
+            this.$textarea.focus();
+        };
+        Object.defineProperty(MdEditorToolbar.prototype, "$textarea", {
+            get: function () {
+                return $('textarea#md-inp');
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(MdEditorToolbar.prototype, "text", {
+            get: function () {
+                return this._text || '';
+            },
+            set: function (value) {
+                this._text = value;
+            },
+            enumerable: true,
+            configurable: true
+        });
         MdEditorToolbar = __decorate([
             trace_1.trace,
             named_1.named('MdEditorToolbar'), 
--- static/js/app/ui/md-editor-toolbar.ts
+++ static/js/app/ui/md-editor-toolbar.ts
@@ -6,6 +6,7 @@ console.debug('[import:app/ui/md-editor-toolbar.ts]');
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
+import {mine} from '../decorator/mine';
 import {named} from '../decorator/named';
 import {trace} from '../decorator/trace';
 
@@ -29,33 +30,127 @@ export class MdEditorToolbar {
             scrollbars: true
         });
 
-        $('.glyphicon-scissors').closest('button')
-            .on('click', this.onScissorsClick.bind(this));
-        $('.glyphicon-duplicate').closest('button')
-            .on('click', this.onDuplicateClick.bind(this));
-        $('.glyphicon-paste').closest('button')
-            .on('click', this.onPasteClick.bind(this));
+        $('.glyphicon.undo').closest('button')
+            .on('click', this.onUndoClick.bind(this));
+        $('.glyphicon.redo').closest('button')
+            .on('click', this.onRedoClick.bind(this));
         $('.glyphicon-erase').closest('button')
             .on('click', this.onEraseClick.bind(this));
+        $('.glyphicon-scissors').closest('button')
+            .on('click', this.onScissorsClick.bind(this));
+        $('.glyphicon-copy').closest('button')
+            .on('click', this.onCopyClick.bind(this));
+        $('.glyphicon-paste').closest('button')
+            .on('click', this.onPasteClick.bind(this));
+
+        $('.glyphicon-indent-left').closest('button')
+            .on('click', this.onIndentLeftClick.bind(this));
+        $('.glyphicon-indent-right').closest('button')
+            .on('click', this.onIndentRightClick.bind(this));
     }
 
     public refresh() {
         this._scroll.refresh();
     }
 
-    private onScissorsClick(ev:MouseEvent) {
+    private onUndoClick(ev:MouseEvent) {
+        this.$textarea.one('focus', () => {
+            let result = document.execCommand('undo');
+            if (result !== true) {
+                console.debug('[on:undo]'); // @TODO: implement!
+            }
+        });
+        this.$textarea.focus();
     }
 
-    private onDuplicateClick(ev:MouseEvent) {
-    }
-
-    private onPasteClick(ev:MouseEvent) {
+    private onRedoClick(ev:MouseEvent) {
+        this.$textarea.one('focus', () => {
+            let result = document.execCommand('redo');
+            if (result !== true) {
+                console.debug('[on:redo]'); // @TODO: implement!
+            }
+        });
+        this.$textarea.focus();
     }
 
     private onEraseClick(ev:MouseEvent) {
+        this.$textarea.one('focus', () => {
+            let result = document.execCommand('delete');
+            if (result !== true) {
+                console.debug('[on:erase]'); // @TODO: implement!
+            }
+        });
+        this.$textarea.focus();
+    }
+
+    private onScissorsClick(ev:MouseEvent) {
+        this.$textarea.one('focus', () => {
+            let beg = this.$textarea[0].selectionStart,
+                end = this.$textarea[0].selectionEnd,
+                val = this.$textarea[0].value;
+            this._text = val.slice(beg, end);
+
+            let result = document.execCommand('cut');
+            if (result !== true) {
+                console.debug('[on:cut]'); // @TODO: implement!
+            }
+        });
+        this.$textarea.focus();
+    }
+
+    private onCopyClick(ev:MouseEvent) {
+        this.$textarea.one('focus', () => {
+            let beg = this.$textarea[0].selectionStart,
+                end = this.$textarea[0].selectionEnd,
+                val = this.$textarea[0].value;
+            this.text = val.slice(beg, end);
+
+            let result = document.execCommand('copy');
+            if (result !== true) {
+                console.debug('[on:copy]'); // @TODO: implement!
+            }
+        });
+        this.$textarea.focus();
+    }
+
+    private onPasteClick(ev:MouseEvent) {
+        this.$textarea.one('focus', () => {
+            let result = document.execCommand('insertText', true, this.text);
+            if (result !== true) {
+                console.debug('[on:insertText]'); // @TODO: implement!
+            }
+        });
+        this.$textarea.focus();
+    }
+
+    private onIndentLeftClick(ev:MouseEvent) {
+        this.$textarea.one('focus', () => {
+             console.debug('[on:focus]'); // @TODO: implement!
+        });
+        this.$textarea.focus();
+    }
+
+    private onIndentRightClick(ev:MouseEvent) {
+        this.$textarea.one('focus', () => {
+             console.debug('[on:focus]'); // @TODO: implement!
+        });
+        this.$textarea.focus();
+    }
+
+    private get $textarea() {
+        return $('textarea#md-inp');
+    }
+
+    private get text():string {
+        return this._text || '';
+    }
+
+    private set text(value:string) {
+        this._text = value;
     }
 
     private _scroll:any;
+    private _text:string;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
