33ef8975c79cd75c35fdd8912921d8c4d69d6d18
Hasan Karahan <hasan.karahan@blackhan.com>
2020-03-21 18:53:38 +0300

    Editor: skip replace via ALT+ENTER

--- static/js/app/ui/md-editor.ts
+++ static/js/app/ui/md-editor.ts
@@ -611,9 +611,11 @@ export class MdEditor {
         altKey: boolean, ctrlKey: boolean, shiftKey: boolean
     }) {
         if (!options || !options.ctrlKey) {
-            let beg_value = this.getValue(new Index(0), new Index(this.index||0));
-            let end_value = this.getValue(new Index(this.index||0));
-            this.setValue(beg_value + end_value.replace(query, new_value));
+            if (!options || !options.altKey) {
+                let beg_value = this.getValue(new Index(0), new Index(this.index||0));
+                let end_value = this.getValue(new Index(this.index||0));
+                this.setValue(beg_value + end_value.replace(query, new_value));
+            }
             this.select(query, !options?.shiftKey ? '+' : '-');
         } else {
             let { lhs, rhs, value } = this.getSelection();
