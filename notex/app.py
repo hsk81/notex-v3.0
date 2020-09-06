8e187d7bc814e338db766bd850422da3d60fb5be
Hasan Karahan <hasan.karahan@blackhan.com>
2020-09-06 16:19:35 +0300

    App: /ipfs => ipfs.notex.ch/ipfs

--- notex/app.py
+++ notex/app.py
@@ -9,6 +9,7 @@ from notex.view.debug import app_debug
 from notex.view.editor import app_edit
 from notex.view.i18n import app_i18n
 from notex.view.index import app_index
+from notex.view.ipfs import app_ipfs
 from notex.view.login import app_login
 from notex.view.now import app_now
 from notex.view.static import app_static
@@ -22,6 +23,7 @@ app_main.merge(app_components)
 app_main.merge(app_edit)
 app_main.merge(app_i18n)
 app_main.merge(app_index)
+app_main.merge(app_ipfs)
 app_main.merge(app_login)
 app_main.merge(app_now)
 app_main.merge(app_static)
