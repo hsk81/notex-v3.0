5de8369212d4fec8237aa263ecc5a6478c62dd71
Hasan Karahan <hasan.karahan@blackhan.com>
2020-10-04 17:00:01 +0300

    Publish: ipfs.add({ content })

--- static/js/app/commands/publish-blog.ts
+++ static/js/app/commands/publish-blog.ts
@@ -6,7 +6,7 @@ import { Command } from "./index";
 import { Ui } from "../ui/index";
 
 import { gateway, html } from "../ipfs/index";
-import { IPFS, Buffer } from "../ipfs/index";
+import { IPFS } from "../ipfs/index";
 import { QRCode } from "../qr-code/index";
 
 export class PublishBlog implements Command {
@@ -21,34 +21,29 @@ export class PublishBlog implements Command {
             $(this).trigger('publishing');
         }
         if (this.altKey || this.meta) {
+            const ipfs = await IPFS.me();
             const head = this.template_dlg.getHead({ title: this.ed.title });
             const body = this.ui.$viewer.contents().find('body').html();
-            const buffer = Buffer.from(await html(head, body));
-            IPFS.me(async (ipfs: any) => {
-                for await (
-                    const item of ipfs.add(buffer)
-                ) {
-                    if (this.meta) {
-                        this.certify(
-                            `${gateway.get()}`, `${item.cid}`, this.meta
-                        ).then(({
-                            tx, cert
-                        }) => {
-                            $(this).trigger('certified', {
-                                tx, cert, post_url
-                            });
-                        }).catch((ex) => {
-                            $(this).trigger('rejected');
-                            console.error('[nft]', ex);
-                        });
-                    } else {
-                        $(this).trigger('published');
-                    }
-                    const post_url = `${gateway.get()}/${item.cid}`;
-                    const tab = window.open(post_url, '_same');
-                    if (tab && tab.focus) tab.focus();
-                }
-            });
+            const item = await ipfs.add({ content: await html(head, body) });
+            if (this.meta) {
+                this.certify(
+                    `${gateway.get()}`, `${item.cid}`, this.meta
+                ).then(({
+                    tx, cert
+                }) => {
+                    $(this).trigger('certified', {
+                        tx, cert, post_url
+                    });
+                }).catch((ex) => {
+                    $(this).trigger('rejected');
+                    console.error('[nft]', ex);
+                });
+            } else {
+                $(this).trigger('published');
+            }
+            const post_url = `${gateway.get()}/${item.cid}`;
+            const tab = window.open(post_url, '_same');
+            if (tab && tab.focus) tab.focus();
         } else {
             this.ui.$publishDialog.modal();
         }
@@ -61,35 +56,30 @@ export class PublishBlog implements Command {
         const content_url = `${ipfs_gateway}/${cid}`;
         meta.content = content_url;
         meta.image = await this.image_url(ipfs_gateway, content_url);
-        const buffer = Buffer.from(JSON.stringify(meta, null, 2));
-        return IPFS.me().then(async (ipfs: any) => {
-            for await (const item of ipfs.add(buffer)) {
-                const cert_url = `${ipfs_gateway}/${item.cid}`;
-                if (this.eth_supported) {
-                    const tx = await this.eth.nft(cert_url);
-                    if (tx) return {
-                        cert: { meta, url: cert_url }, tx
-                    };
-                } else {
-                    return {
-                        cert: { meta, url: cert_url }, tx: undefined
-                    };
-                }
-            }
-            throw null;
-        });
+        const content = JSON.stringify(meta, null, 2);
+        const ipfs = await IPFS.me();
+        const item = await ipfs.add({ content });
+        const cert_url = `${ipfs_gateway}/${item.cid}`;
+        if (this.eth_supported) {
+            const tx = await this.eth.nft(cert_url);
+            if (tx) return {
+                cert: { meta, url: cert_url }, tx
+            };
+        } else {
+            return {
+                cert: { meta, url: cert_url }, tx: undefined
+            };
+        }
+        throw null;
     }
     private image_url(
         ipfs_gateway: string, ipfs_url: string
     ) {
         return new Promise<string>((resolve) => {
-            QRCode(ipfs_url).then((svg) => {
-                const buffer = Buffer.from(svg);
-                IPFS.me().then(async (ipfs: any) => {
-                    for await (const item of ipfs.add(buffer)) {
-                        resolve(`${ipfs_gateway}/${item.cid}`);
-                    }
-                });
+            QRCode(ipfs_url).then(async (svg) => {
+                const ipfs = await IPFS.me();
+                const item = await ipfs.add({ content: svg });
+                resolve(`${ipfs_gateway}/${item.cid}`);
             });
         });
     }
