bc55a93827a96ce4a9f7e5db7c73066b13f35ed6
Hasan Karahan <hasan.karahan@blackhan.com>
2021-03-21 20:30:14 +0300

    Publish: TOC w/nft-{ main, extra }

--- static/js/app/app-nfts.ts
+++ static/js/app/app-nfts.ts
@@ -86,8 +86,6 @@ export class NFTs {
         if (!address) return;
         let uris = await ntxc.tokenURIs(address);
         if (!uris || !uris.length) return;
-        const n_cols = this.$colums.length;
-        const n_cols_visible = this.$thead.find('th:visible').length;
         for (const uri of uris.sort((lhs, rhs) => {
             return parseInt(lhs.id) > parseInt(rhs.id) ? +1 : -1;
         })) {
@@ -99,9 +97,12 @@ export class NFTs {
                         certificate
                     )}`);
                 }
-                this.$tbody.append(row(uri, certificate));
+                const nft_main = get_nft_main(uri, certificate);
+                if (nft_main) this.$tbody.append(nft_main);
+                const nft_extra = get_nft_extra(uri, certificate);
+                if (nft_extra) this.$tbody.append(nft_extra);
             }).catch((reason) => {
-                this.$tbody.append(row(uri, {
+                this.$tbody.append(get_nft_main(uri, {
                     authors: [],
                     content: '',
                     description: '',
@@ -114,62 +115,94 @@ export class NFTs {
                 this.$tbody.find('tr#placeholder').remove();
             });
         }
-        function row(
+        function get_nft_main(
             uri: { id: string, value: string }, certificate: PdfCertificateMeta
         ) {
-            const cols = [`<tr id="${uri.id}">`];
+            const cols = [`<tr class="nft-main" id="${uri.id}">`];
             if (certificate.content) {
-                cols.push(`<th scope="row">
-                    <a href="${certificate.content}" target="_blank">${uri.id}</a>
-                </th>`);
-                cols.push(`<th class="d-none d-sm-table-cell">${certificate.image
+                cols.push(`<td scope="row">
+                    <a href="${certificate.content}" target="_blank">#${uri.id}</a>
+                </td>`);
+                cols.push(`<td class="d-none d-sm-table-cell">${certificate.image
                     ? `<img src="${certificate.image}" style="border-radius: 0.25em;" alt="QR Code">`
                     : ''
-                    }</th>`);
+                }</td>`);
                 cols.push(`<td>
-                    <p title="${certificate.name}">
-                        ${certificate.name}
+                    <p title="${certificate.name}" style="max-width:15em;">
+                        <a href="${certificate.content}" target="_blank">${certificate.name}</a>
                     </p>
                 </td>`);
                 cols.push(`<td>
-                    <p title="${certificate.description}">
+                    <p title="${certificate.description}" class="d-none d-sm-table-cell" style="max-width:20em;">
                         ${certificate.description}
                     </p>
                 </td>`);
-                cols.push(`<td class="d-none d-sm-table-cell">
-                    <p title="${certificate.authors?.join(', ') ?? ''}">
-                        ${certificate.authors?.join(', ') ?? ''}
-                    </p>
-                </td>`);
-                cols.push(`<td class="d-none d-sm-table-cell">
-                    <p title="${certificate.emails?.join(', ') ?? ''}">
-                        ${certificate.emails?.join(', ') ?? ''}
-                    </p>
-                </td>`);
-                cols.push(`<td class="d-none d-sm-table-cell">
-                    <p title="${certificate.keywords?.join(', ') ?? ''}">
-                        ${certificate.keywords?.join(', ') ?? ''}
-                    </p>
-                </td>`);
-
             } else {
                 cols.push(`<th scope="row">
                     <a class="btn-link disabled" target="_blank">${uri.id}</a>
                 </th>`);
-                cols.push(`<td colspan="${6 - (n_cols - n_cols_visible)}">
+                cols.push(`<td colspan="3">
                     <em style="display: block; text-align: center;">publication metadata not available</em>
                 </td>`);
             }
-            cols.push(`<td>
-                <button title="Burn Certificate" type="button" class="btn btn-outline-danger btn-sm burn">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+            cols.push(`<td style="max-width:100px;">`);
+            cols.push(`<button title="Burn" type="button" class="btn btn-outline-danger btn-sm burn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+            </button>`);
+            if (certificate.content) {
+                cols.push(`<button title="Expand" type="button" class="btn btn-outline-dark btn-sm toggle d-none d-sm-table-cell">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
                     </svg>
-                </button>
-            </td>`);
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16" style="display: none;">
+                        <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
+                    </svg>
+                </button>`);
+            }
+            cols.push(`</td>`);
             cols.push(`</tr>`);
             return cols.join('\n');
+        }
+        function get_nft_extra(
+            uri: { id: string, value: string }, certificate: PdfCertificateMeta
+        ) {
+            if (certificate.content) {
+                const rows = [];
+                rows.push(`<tr class="nft-extra nft-authors" style="display: none;">`);
+                rows.push(`<td colspan="2">
+                    <p style="font-weight: bold; text-align: right;">Author(s):</p>
+                </td>`);
+                rows.push(`<td colspan="3">
+                    <p title="${certificate.authors?.join(', ') ?? ''}" style="max-width: 38em;">
+                        ${certificate.authors?.join(', ') ?? ''}
+                    </p>
+                </td>`);
+                rows.push(`</tr>`);
+                rows.push(`<tr class="nft-extra nft-emails" style="display: none;">`);
+                rows.push(`<td colspan="2">
+                    <p style="font-weight: bold; text-align: right;">Email(s):</p>
+                </td>`);
+                rows.push(`<td colspan="3">
+                    <p title="${certificate.emails?.join(', ') ?? ''}" style="max-width: 38em;">
+                        ${certificate.emails?.join(', ') ?? ''}
+                    </p>
+                </td>`);
+                rows.push(`</tr>`);
+                rows.push(`<tr class="nft-extra nft-keywords" style="display: none;">`);
+                rows.push(`<td colspan="2">
+                    <p style="font-weight: bold; text-align: right;">Keyword(s):</p>
+                </td>`);
+                rows.push(`<td colspan="3">
+                    <p title="${certificate.keywords?.join(', ') ?? ''}" style="max-width: 38em;">
+                        ${certificate.keywords?.join(', ') ?? ''}
+                    </p>
+                </td>`);
+                rows.push(`</tr>`);
+                return rows.join('\n');
+            }
         }
     }
     private events() {
@@ -177,8 +210,28 @@ export class NFTs {
         $install.on('click', this.on_install_click.bind(this));
         const $connect = this.$wallet_connect.find('.lhs,.mid');
         $connect.on('click', this.on_connect_click.bind(this));
-        const $buttons = this.$tbody.find('button.burn');
-        $buttons.on('click', this.on_burn_click.bind(this));
+        const burn = this.$tbody.find('button.burn');
+        burn.on('click', this.on_burn_click.bind(this));
+        const toggle = this.$tbody.find('button.toggle');
+        toggle.on('click', this.on_toggle_click.bind(this));
+    }
+    private async on_toggle_click(ev: JQuery.ClickEvent) {
+        const $row = $(ev.target).closest('tr');
+        const $toggle = $row.find('button.toggle');
+        const state = $row.data('state');
+        if (state !== 'expanded') {
+            $row.nextAll('.nft-extra').slice(0, 3).show();
+            $toggle.find('svg.bi-chevron-down').hide();
+            $toggle.find('svg.bi-chevron-up').show();
+            $toggle.attr('title', 'Collapse');
+            $row.data('state', 'expanded');
+        } else {
+            $row.nextAll('.nft-extra').slice(0, 3).hide();
+            $toggle.find('svg.bi-chevron-down').show();
+            $toggle.find('svg.bi-chevron-up').hide();
+            $toggle.attr('title', 'Expand');
+            $row.data('state', '');
+        }
     }
     private on_install_click(ev: JQuery.ClickEvent) {
         const tab = window.open('https://metamask.io/', '_black');
